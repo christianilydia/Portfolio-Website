5ed27302f53b65fbf88030edeafe393220e09ef5
christiani lydia <christiani.lydia7@gmail.com>
2018-09-03 21:33:50 -0700

    Add mouseover and mouseout action Add fade in and fade out

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 console.log("Hi, Welcome to my portfolio site")
+
+$(".item").on("mouseover", function() {
+  $(this).parent().addClass("is-hovered");
+});
+$(".item").on("mouseout", function() {
+  $(this).parent().removeClass("is-hovered");
+});
+
+$(document).ready(function() {
+  $(".project").on("mouseenter", function() {
+    $(this).find(".project-details").fadeIn();
+    });
+  $(".project").on("mouseleave", function() {
+    $(this).find(".project-details").fadeOut();
+    });
+});
